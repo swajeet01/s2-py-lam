bd0364178ed914afe072657c23ce9c2420dd5681
swapfile3 <swajeetg768@gmail.com>
2020-12-05 13:10:57 +0530

    Try broadcasting newly joined clients

--- client.py
+++ client.py
@@ -1,29 +1,41 @@
 import socket
 import select
 import sys
+import threading
 
 if len(sys.argv) != 3:
-    print("Usage: python3 client.py <IP> <port>")
-    exit(1)
+    print("Usage: python3 server.py <IP> <PORT>")
+    print("Now binding on default IP(127.0.0.1) and PORT(7066)")
+    IP = "127.0.0.1"
+    PORT = 7066
+else:
+    IP = str(sys.argv[1])
+    PORT = int(sys.argv[2])
 
-IP = str(sys.argv[1])
-PORT = int(sys.argv[2])
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.connect((IP, PORT))
+# TODO: name it idiot
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, PORT))
 
 name = input("Your name: ")
+s.send(name.encode("utf-8"))
+
+stream_list = [sys.stdin, s]
 
 while True:
-    sockets_list = [sys.stdin, server_socket]
-    read_sockets, write_sockets, exception_sockets = select.select(sockets_list, [], [])
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            msg = notified_socket.recv(1024)
-            print(msg.decode("utf-8"), end='')
-        else:
-            msg = input()
-            if msg.lower() == 'bye':
-                server_socket.close()
-                exit(1)
-            server_socket.send(("\n" + name + ": " + msg).encode("utf-8"))
+    try:
+        read_sockets, write_sockets, exception_sockets = select.select(stream_list, [], [])
+        for notified_socket in read_sockets:
+            if notified_socket == s:
+                msg = notified_socket.recv(1024)
+                print(msg.decode("utf-8"))
+            else:
+                msg = input()
+                if msg.lower() == "c:bye":
+                    s.close()
+                    print("You left")
+                    exit(1)
+                s.send((name + ": " + msg).encode("utf-8"))
+    except (KeyboardInterrupt, EOFError):
+        s.close()
+        print("You left")
+        exit(1)
--- server2.py
+++ server2.py
@@ -3,13 +3,13 @@ import sys
 import threading
 
 if len(sys.argv) != 3:
-    print("Usage: python3 server.py <IP> <port>")
-    exit(1)
-
-HEADER_LENGTH = 25
-
-IP = str(sys.argv[1])
-PORT = int(sys.argv[2])
+    print("Usage: python3 server.py <IP> <PORT>")
+    print("Now binding on default IP(127.0.0.1) and PORT(7066)")
+    IP = "127.0.0.1"
+    PORT = 7066
+else:
+    IP = str(sys.argv[1])
+    PORT = int(sys.argv[2])
 
 # Creating a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,50 +27,59 @@ except socket.gaierror as err:
 
 # Listen for new connections
 server_socket.listen()
-sockets_list = []
+socket_list = []
+address_info_list = []
 
 print(f'Listening for connections on {IP}:{PORT}...')
 
 
 # To pass to thread
-def client_callback(client_socket, info):
-    client_socket.send("Welcome to LAM\nYou: ".encode("utf-8"))
+def client_callback(_client_socket, _info):
+    name = _client_socket.recv(1024)
+    broadcast(name.decode("utf-8") + " has joined the chat", _client_socket)
+    _client_socket.send("Welcome to LAM\n".encode("utf-8"))
     while True:
-        msg = client_socket.recv(1024)
-        if msg:
-            msg_str = msg.decode("utf-8")
-            print(msg_str, end='')
-            broadcast_msg = msg_str
-            broadcast(broadcast_msg, client_socket)
-        else:
-            remove(client_socket)
+        try:
+            msg = _client_socket.recv(1024)
+            if msg:
+                msg_str = msg.decode("utf-8")
+                print(msg_str)
+                broadcast_msg = msg_str
+                broadcast(broadcast_msg, _client_socket)
+            else:
+                remove(_client_socket)
+                print("client disconnected")
+                break
+        except:
+            remove(_client_socket)
+            print("client disconnected")
+            return
 
 
 def broadcast(msg, sender):
-    for socks in sockets_list:
+    for socks in socket_list:
         if socks != sender:
             try:
-                socks.send((msg + "\nYou: ").encode("utf-8"))
+                socks.send(msg.encode("utf-8"))
             except:
                 socks.close()
                 remove(socks)
 
 
-def remove(client_socket):
-    if client_socket in sockets_list:
-        sockets_list.remove(client_socket)
+def remove(_client_socket: socket):
+    if _client_socket in socket_list:
+        socket_list.remove(_client_socket)
 
 
 while True:
     try:
         (client_socket, info) = server_socket.accept()
-        sockets_list.append(client_socket)
-        print("Log: A new user connected")
+        socket_list.append(client_socket)
         client_thread = threading.Thread(target=client_callback, args=(client_socket, info))
         client_thread.start()
-        broadcast("New user connected", client_socket)
     except KeyboardInterrupt:
         print("\nShutting down server...")
+        exit(0)
         break
 
 server_socket.close()
